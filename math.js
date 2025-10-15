c0d19f71e3bb9183b9699e94d14201adc34fba92
Chinmayee Maharana <maharanachinmayee1@gmail.com>
2025-10-15 14:45:38 +0530

    Add files via upload

---
+++ math.js
@@ -0,0 +1,59 @@
+let inputA = '';
+let operator = '';
+let inputB = '';
+
+function appendNumber(number) {
+    inputA += number;
+    document.getElementById('display').value = `${inputB} ${operator} ${inputA}`;
+}
+
+function appendOperation(op) {
+    if (inputA === '') return;
+    if (inputB !== '') {
+        calculate(); 
+    }
+    operator = op;
+    inputB = inputA;
+    inputA = '';
+    document.getElementById('display').value = `${inputB} ${operator}`;
+}
+
+function calculate() {
+    if (inputB === '' || inputA === '') return;
+    let result;
+    let b = parseFloat(inputB);
+    let a = parseFloat(inputA);
+
+    switch (operator) {
+        case '+':
+            result = b + a;
+            break;
+        case '-':
+            result = b - a;
+            break;
+        case '*':
+            result = b * a;
+            break;
+        case '/':
+            if (a === 0) {
+                document.getElementById("display").value = "ERROR";
+                return;
+            }
+            result = b / a;
+            break;
+        default:
+            return;
+    }
+
+    inputA = result.toString();
+    operator = '';
+    inputB = '';
+    document.getElementById('display').value = inputA;
+}
+
+function clearDisplay() {
+    inputA = '';
+    inputB = '';
+    operator = '';
+    document.getElementById('display').value = '';
+}
